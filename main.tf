9f88f91ca7c954256b2225938a6f91a579b4209c
jmallatt-kiva <jmallatt@kiva.org>
2025-08-25 11:09:41 -0700

    set ecr account upstream

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "initial_load" {
   source_table_name      = var.source_table_name
   stage                  = var.stage
   stage_type             = var.stage_type
-  ecr_repo_account       = "111644114322"
+  ecr_repo_account       = var.ecr_repo_account
 }
 
 module "ongoing_replication" {
